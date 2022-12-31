9891bcc6abbef652369ded16f9a45fa5c277db98
ViXbob <vixbob@outlook.com>
2023-01-01 03:47:19 +0800

    normalize n_1, n_2

--- include/ray.h
+++ include/ray.h
@@ -21,6 +21,7 @@ struct Ray {
   , direction(dir.normalized())
   , t_min(t_min)
   , t_max(t_max) {
+		direction.normalize();
     inv_direction = {
       std::fabs(direction[0]) < (float)1e-10 ? (float)1e10 : 1.f / direction[0],
       std::fabs(direction[1]) < (float)1e-10 ? (float)1e10 : 1.f / direction[1],
@@ -29,10 +30,12 @@ struct Ray {
 		if(std::fabs(direction.x()) > std::fabs(direction.y()) && 
 			 std::fabs(direction.x()) > std::fabs(direction.z())) {
 			n_1 = {direction.y(), -direction.x(), 0.f};
+			n_1.normalize();
 		} else {
 			n_1 = {0.f, direction.z(), -direction.y()};
+			n_1.normalize();
 		}
-		n_2 = n_1.cross(direction);
+		n_2 = n_1.cross(direction).normalized();
 		d_1 = -n_1.dot(origin);
 		d_2 = -n_2.dot(origin);
   }
